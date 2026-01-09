441bb0503e6687b26daa7d01b08b7be53abe0e91
dias123ve <diassyahrian3004@gmail.com>
2026-01-10 04:34:16 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import Index from "./pages/Index";
 import LoginPage from "./pages/login";
 import DashboardPage from "./pages/dashboard";
 import GeneratePage from "./pages/generate";
@@ -11,26 +12,27 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          {/* Auth */}
-          <Route path="/login" element={<LoginPage />} />
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            {/* Legacy / bebas */}
+            <Route path="/" element={<Index />} />
 
-          {/* Main */}
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/generate/:master_id" element={<GeneratePage />} />
+            {/* Fokus utama */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/generate/:master_id" element={<GeneratePage />} />
 
-          {/* Fallback */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+            {/* Safety */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
