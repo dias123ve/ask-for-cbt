ee5f372c4fffc1d3bba3745e4ea99d947c5fa2ba
dias123ve <diassyahrian3004@gmail.com>
2026-01-10 04:14:11 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+
+import LoginPage from "./pages/login";
+import DashboardPage from "./pages/dashboard";
+import GeneratePage from "./pages/generate";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -15,8 +18,14 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Auth */}
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* Main */}
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/generate/:master_id" element={<GeneratePage />} />
+
+          {/* Fallback */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
